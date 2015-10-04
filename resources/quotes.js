0e26ee88a9e96a10dc74df1b52abaf6e894bc530
Sarthak Garg <gargsarthak30@gmail.com>
2015-10-04 20:51:13 +0530

    quote updated

--- resources/quotes.js
+++ resources/quotes.js
@@ -1,4 +1,4 @@
 document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'>The roots of education are bitter, but the fruit is sweet.</dt>');
-document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>Aristotle</a></font></b></dd><p> </p>');
+document.writeln('<dt class=\'tqpQuote\'>The measure of who we are is what we do with what we have.</dt>');
+document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>Vince Lombardi</a></font></b></dd><p> </p>');
 document.writeln('</dl>');
