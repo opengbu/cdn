ea0223cac9e4d5b346f62b44d1e1daf44ffe49d0
Rajat Saxena <rajat.saxena035@gmail.com>
2015-10-06 19:20:07 +0530

    quote updated

--- resources/quotes.js
+++ resources/quotes.js
@@ -1,4 +1,4 @@
 document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'>Better than a thousand hollow words, is one word that brings peace.</dt>');
-document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>Buddha</a></font></b></dd><p> </p>');
+document.writeln('<dt class=\'tqpQuote\'> Life is about making an impact, not making an income. </dt>');
+document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>–Kevin Kruse</a></font></b></dd><p> </p>');
 document.writeln('</dl>');
