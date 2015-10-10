1b44f7e18e7e45cf2ac0e72eae7ca3f2eb41afdf
Sarthak Garg <gargsarthak30@gmail.com>
2015-10-10 16:01:02 +0530

    quote updated

--- resources/quotes.js
+++ resources/quotes.js
@@ -1,4 +1,4 @@
 document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'> Life is about making an impact, not making an income. </dt>');
-document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>–Kevin Kruse</a></font></b></dd><p> </p>');
+document.writeln('<dt class=\'tqpQuote\'> “If A is a success in life, then A equals x plus y plus z. Work is x; y is play; and z is keeping your mouth shut”  </dt>');
+document.writeln('<dd> <b><font size=\'-1\'><a class=\'tqpAuthor\' target=\'_blank\'>– Albert Einstein</a></font></b></dd><p> </p>');
 document.writeln('</dl>');
