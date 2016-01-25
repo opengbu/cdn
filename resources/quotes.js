7d21359c269ebe74e45ecb51ad7405288ca57930
Rajat Saxena <rajat.saxena035@gmail.com>
2016-01-25 22:57:27 +0530

    randomised quotes

--- resources/quotes.js
+++ resources/quotes.js
@@ -1,4 +1,19 @@
+var q = [
+			{"quote":"Some succeed because they are destined to, but most succeed because they are determined to.", "author":"Henry Van Dyke"},
+			{"quote":"Success is nothing more than a few simple disciplines, practiced every day.", "author":"Jim Rohn"},
+			{"quote":"Life is 10% what happens to you and 90% how you react to it.", "author":"Charles R. Swindoll"},
+			{"quote":"A real friend is one who walks in when the rest of the world walks out.", "author":"Walter Winchell"},
+			{"quote":"Don't watch the clock; do what it does. Keep going.", "author":"Sam Levenson"},
+			{"quote":"The purpose of education is to replace an empty mind with an open one.", "author":"Malcolm Forbes"},
+			{"quote":"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.", "author":"Thomas Edison"},
+			{"quote":"Give me six hours to chop down a tree and I will spend the first four sharpening the axe.", "author":"Abraham Lincoln"},
+			{"quote":"If you don’t build your dream, someone else will hire you to help them build theirs.", "author":"Dhirubhai Ambani"}
+		];
+
+var range = q.length;
+var random = Math.floor(Math.random() * range);
+
 document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'> “Some succeed because they are destined to, but most succeed because they are determined to.”</dt>');
-document.writeln('<dd> <b><font size=\'-1\' color = \'blue\'>– Henry Van Dyke</font></b></dd><p> </p>');
+document.writeln('<dt class=\'tqpQuote\'> “'+q[random].quote+'”</dt>');
+document.writeln('<dd> <b><font size=\'-1\' color = \'blue\'>– '+q[random].author+'</font></b></dd><p> </p>');
 document.writeln('</dl>');
